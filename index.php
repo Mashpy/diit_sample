c621dbc8b0ac788eb591131282c2fcd2707aae6c
Mashpy Says <likewarid@gmail.com>
2014-02-27 14:20:34 +0600

    inserting text into xml file

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
+<html>
+<head><title>test</title></head>
+</head>
+
 <?php
-    phpinfo();
-	echo "again test";
+    $xmldoc = new DOMDocument();
+    $xmldoc->load('sample.xml', LIBXML_NOBLANKS);
+
+    $activities = $xmldoc->firstChild->firstChild;
+    if($activities!=null){
+        while($activities!=null){
+            echo $activities->textContent.'<br/>';
+            $activities = $activities->nextSibling;
+        }
+    }
 ?>
+
+<form name='input' action='insert.php' method='post'>
+    insert activity:
+    <input type='text' name='activity'/>
+    <input type='submit' value='send'/>
+</form>
+</body>
+</html>
