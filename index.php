67de0e9383122aba5e17d4ca9886c51a9f1104ea
Muhammad Minhazul Haque <contact@minhazulhaque.com>
2014-02-28 01:57:13 +0600

    fixed xml structure

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
+<!DOCTYPE html>
 <html>
-<head><title>test</title></head>
-</head>
-
-<?php
-if( ! $xml = simplexml_load_file('sample.xml') )
-    {
-        echo 'unable to load XML file';
-    }
-    else
-    {
-        foreach( $xml->workers as $workers )
-        {
-            echo '***Name: <b>'.$workers->name.'</b><br />';
-                        echo 'sunday: '.$workers->sunday.'<br />';
-                        echo 'monday: '.$workers->monday.'<br />';
-                        echo 'tuesday: '.$workers->tuesday.'<br />';
-                        echo 'wednesday: '.$workers->wednesday.'<br />';
-                        echo 'thusday: '.$workers->thusday->getAttribute("type");
-
-        }
-        foreach( $xml->types as $type )
-        {
-            echo '***ID: <b>'.$type->id.'</b><br />';
-                        echo 'typename: '.$type->typename.'<br />';
-
-        }
-        
-		
-		
-    } 
-?>
- 
-<form name='input' action='insert.php' method='post'>
-    insert activity:
-    <input type='text' name='activity'/>
-    <input type='submit' value='send'/>
-</form>
-</body>
+    <title>DIIT</title>
+    <style> table
+{
+border-collapse:collapse;
+}
+table,th, td
+{
+border: 1px solid black;
+}</style>
+    <body>
+        <table width="500px">
+            <thead>
+                <th>ID</th>
+                <th>Type</th>
+                <th>Name</th>
+                <th>Sun</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Total</th>
+            </thead>
+            <tbody>
+            <?php
+                $xml = simplexml_load_file('sample.xml');
+                foreach($xml->worker as $worker)
+                {
+                    $total = 0;
+                    echo '<tr>'.PHP_EOL;
+                    foreach($worker->attributes() as $key => $value)
+                    {
+                        if($key[0]=='d')
+                            $total += intval($value);
+                        echo '<td>'.$value.'</td>'.PHP_EOL;
+                    }
+                    echo '<td>'.$total.'</td>'.PHP_EOL;
+                    echo '</tr>'.PHP_EOL;
+                }
+            ?>
+            </tbody>
+        </table>
+        <form name='input' action='insert.php' method='post'>
+            insert activity:
+            <input type='text' name='activity'/>
+            <input type='submit' value='send'/>
+        </form>
+    </body>
 </html>
